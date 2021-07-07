5cc90eed0d33f253b08d73409abcd302fcb33a65
palinz <82084953+palinz@users.noreply.github.com>
2021-07-07 13:47:11 +0800

    update cetak.blade.php 1

--- resources/views/laporan/cetak.blade.php
+++ resources/views/laporan/cetak.blade.php
@@ -11,8 +11,9 @@
     <h5 align=center>MINIT CURAI</h5>
     <h5 align=center>MESYUARAT LUAR / BENGKEL / KURSUS / PROGRAM</h5>
     <h5 align=center>JABATAN PENDIDIKAN NEGERI MELAKA</h5>
+    <hr style="width:100%;text-align:center;margin-left:0">
     <br>
-	<hr style="width:100%;text-align:center;margin-left:0">
+	
     <table width="100%" border="" align="center">
         <tr>
             <td>1. &nbsp;&nbsp;&nbsp;
@@ -21,16 +22,19 @@
         <tr>
             <td>{{ $minitCurai->tajuk }}</td>
         </tr>
+	<tr style="border-bottom:1px solid black">
+		<td colspan="100%"></td>
+	</tr>
         <tr>
             <td>2. &nbsp;&nbsp;&nbsp;
                 Anjuran:</td>
         </tr>
-        <tr style="border-bottom:1px solid black">
-			<td colspan="100%"></td>
-		</tr>
-		<tr>
+	<tr>
             <td>{{ $minitCurai->anjuran }}</td>
         </tr>
+	<tr style="border-bottom:1px solid black">
+		<td colspan="100%"></td>
+	</tr>
         <tr>
             <td>3. &nbsp;&nbsp;&nbsp;
                 Tarikh / Masa / Tempat:</td>
@@ -39,19 +43,16 @@
             <td>{{ $minitCurai->tarikh }}, {{ $minitCurai->lokasi }}</td>
         </tr>
         <tr style="border-bottom:1px solid black">
-			<td colspan="100%"></td>
-		</tr>
-		<tr>
+		<td colspan="100%"></td>
+	</tr>
+	<tr>
             <td>4. &nbsp;&nbsp;&nbsp;
                 Pegawai Yang Terlibat:</td>
         </tr>
         <tr>
             <td>{{ $minitCurai->pegawai_terlibat }}</td>
         </tr>
-        <tr style="border-bottom:1px solid black">
-			<td colspan="100%"></td>
-		</tr>
-		<tr>
+       	<tr>
             <td>5. &nbsp;&nbsp;&nbsp;
                 Isu / Isu Penting Mesyuarat luar / Bengkel / Kursus / Program:</td>
         </tr>
@@ -60,17 +61,17 @@
         </tr>
         <tr style="border-bottom:1px solid black">
 			<td colspan="100%"></td>
-		</tr>
-		<tr>
+	</tr>
+	<tr>
             <td>6. &nbsp;&nbsp;&nbsp;
                 Nyatakan Tindakan Yang Mesti / Perlu Diambil:</td>
         </tr>
         <tr>
             <td>{{ $minitCurai->tindakan }}</td>
         </tr>
-		<tr style="border-bottom:1px solid black">
-			<td colspan="100%"></td>
-		</tr>
+	<tr style="border-bottom:1px solid black">
+		<td colspan="100%"></td>
+	</tr>
     </table>
     <br>
     <br>
@@ -121,8 +122,6 @@
             </td>
         </tr>
     </table>
-
-</body>
 <style>
 .footer {
   position: fixed;
@@ -137,5 +136,7 @@
 <div class="footer">
   <p>Hak Milik Jabatan Pendidikan Negeri Melaka</p>
 </div>
+
+</body>
 
 </html>
