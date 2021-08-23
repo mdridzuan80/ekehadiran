44d0e3a6748e2bfe2ccd53ff79ed2470d81224e0
palinz <82084953+palinz@users.noreply.github.com>
2021-08-23 12:22:21 +0800

    Update index.blade.php

--- resources/views/minitcurai/index.blade.php
+++ resources/views/minitcurai/index.blade.php
@@ -335,7 +335,7 @@
                     }
                 });
             });
-               $(".comPegawai").comPegawai();     
+               
         });
 
         $('#modal-edit-minit-curai').on('submit', "#frm-edit-minit-curai", function(e) {
@@ -555,9 +555,7 @@
         });
     });
 </script>
-<script type="text/javascript">
-$(".comPegawai").comPegawai();
-</script>
+
 
 
 
