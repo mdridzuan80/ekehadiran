5bbbe286720b2295a20428e5f3a0f86499cc95cd
palinz <82084953+palinz@users.noreply.github.com>
2021-09-03 14:54:39 +0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>eK</b></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>eKehadiran</b></span>
+        <img src="{{asset('images/logojpnm.png')}}" alt="" width="45"><b>eKehadiran</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
