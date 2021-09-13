740c27490a0b77f897864d548f46f0189db8c2de
palinz <82084953+palinz@users.noreply.github.com>
2021-09-13 10:02:30 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,9 +31,15 @@
     <style>
       .capslockMessage { color: red; }
     </style>
+    <style>
+        body  {
+          background-image: url("images/bg1.jpg");
+          background-color: #cccccc;
+        }
+    </style>
 </head>
 <body class="hold-transition login-page">
-<div class="banner"><img src="{{asset('images/banner3.jpg')}}" alt height="" width="100%"></div>
+  <!-- <div class="banner"><img src="{{asset('images/banner3.jpg')}}" alt height="" width="100%"></div> -->
 <div class="login-box">
   <div class="login-logo">
     <!-- <img src="{{ asset('dist/img/logo_dalam.png') }}" alt="Logo Rasmi eMasa"> -->
