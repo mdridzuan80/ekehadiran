8379211c429d4231cf40f974bafbf1cd7723d883
palinz <82084953+palinz@users.noreply.github.com>
2021-09-21 11:11:44 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -78,7 +78,7 @@
     </form>
   </div>
   <!-- /.login-box-body -->
-  <br><p style="text-align: center">Sila muatturun manual eJPNM <a href="{{ URL::to('images/Manual.pdf') }}">disini</a></p>
+  <br><p style="text-align: center">Sila muatturun manual eJPNM <a href="{{ asset('images/Manual.pdf') }}">disini</a></p>
 </div>
 <!-- /.login-box -->
 
