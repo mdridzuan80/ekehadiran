6d8bb4c15f4216bfd3adc7ad0adc7d95a55bf6c9
palinz <82084953+palinz@users.noreply.github.com>
2021-10-04 16:29:23 +0800

    Update MinitCuraiController.php

--- app/Http/Controllers/MinitCuraiController.php
+++ app/Http/Controllers/MinitCuraiController.php
@@ -131,11 +131,7 @@ class MinitCuraiController extends BaseController
         }
     }
 
-    public function cetak(MinitCurai $minitCurai)
-    {
-        $pdf = PDF::loadView('laporan.cetak', compact('minitCurai'));
-        return $pdf->download('MinitCuraiJPNMelaka.pdf');
-    }
+    
     
     public function destroy(MinitCurai $minitCurai)
     {
